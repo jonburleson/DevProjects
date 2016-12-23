a7f919dbe6604affa25f7d8b62074e4c4e177b2d
jonburleson <jonburleson@live.com>
2016-12-22 19:49:46 -0600

    BinaryClock
    A binary clock for android.

---
+++ BinaryClock/app/src/main/java/com/skyspace/jongabe/binaryclock/Clock.java
@@ -0,0 +1,13 @@
+package com.skyspace.jongabe.binaryclock;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Clock extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_clock);
+    }
+}
